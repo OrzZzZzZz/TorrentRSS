47ad004fa6c4e171a434e5a5ddf6f2d341f7e90f
leekh <leekh529@gmail.com>
2021-08-20 22:13:36 +0900

    first commit

---
+++ torrentview/Torrentview.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace torrentview
+{
+    class Torrentview
+    {
+        private string domain;
+        private string url;
+        private string category;
+        private string subject;
+        private string magnet;
+        static void Main(string[] args)
+        {
+            Torrentview torrentview = new Torrentview();
+        }
+    }
+}
