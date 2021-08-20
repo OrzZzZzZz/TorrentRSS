59c6b72bc167f432f0c594eae6b24e86e3d665f7
leekh <leekh529@gmail.com>
2021-08-20 22:32:46 +0900

    ..

--- torrentview/Torrentview.cs
+++ torrentview/Torrentview.cs
@@ -1,19 +1,20 @@
 ﻿using System;
+
 namespace torrentview
 {
     class Torrentview
     {
         private string domain;
-        private string url;
         private string category;
-        private string subject;
-        private string magnet;
+
         static void Main(string[] args)
         {
             Torrentview torrentview = new Torrentview();
             torrentview.MakeXML();
         }
 
-        private void MakeXML() { }
+        private void MakeXML()
+        {
+        }
     }
 }
