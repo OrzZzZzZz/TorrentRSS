7a5eea377d749875ded77a84cbbf5faac8396fe2
leekh <leekh529@gmail.com>
2021-08-20 23:17:56 +0900

    ..

--- torrentview/Torrentview.cs
+++ torrentview/Torrentview.cs
@@ -1,21 +1,36 @@
 ﻿using System;
+using System.Net;
 
 namespace torrentview
 {
     class Torrentview
     {
-        private string domain;
-        private string category;
-
         static void Main(string[] args)
         {
-            Torrentview torrentview = new Torrentview();
-            torrentview.domain = GetDomain();
+            Torrentview torrent = new Torrentview();
+            string domain = torrent.GetDomain("torrentview");
+            Console.WriteLine(domain);
         }
 
-        private static string GetDomain()
+        private string GetHtml(string empty)
         {
             return "";
+        }
+
+        private string GetDomain(string domain)
+        {
+            var num = 35;
+            for (var i = num; i < 40; i++)
+            {
+                HttpWebRequest request = (HttpWebRequest) WebRequest.Create("http://www." + domain + i + ".com");
+                request.Method = "GET";
+                HttpWebResponse response = (HttpWebResponse) request.GetResponse();
+                HttpStatusCode status = response.StatusCode;
+                Console.WriteLine(status);
+            }
+
+            //domain = domain + "35";
+            return domain;
         }
     }
 }
