5db013ec94626c55b9a51a65b35092ec20e978d8
leekh <leekh529@gmail.com>
2021-08-23 10:12:28 +0900

    ..

---
+++ TorrentRSS/TorrentRSS.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Net;
+
+namespace TorrentRSS
+{
+    class TorrentRSS
+    {
+        static void Main(string[] args)
+        {
+            TorrentRSS torrentRss = new TorrentRSS();
+            string domain = torrentRss.GetDomain("torrentview");
+            Console.WriteLine(domain);
+        }
+
+        string GetHtml(string empty)
+        {
+            return "";
+        }
+
+        string GetDomain(string domain)
+        {
+            int i = 100;
+            while (true)
+            {
+                Console.WriteLine("http://www." + domain + i + ".com");
+                try
+                {
+                    HttpWebRequest request = (HttpWebRequest) WebRequest.Create("http://www." + domain + i + ".com");
+                    request.Method = "GET";
+                    HttpWebResponse response = (HttpWebResponse) request.GetResponse();
+                    if (response.StatusCode == HttpStatusCode.OK)
+                    {
+                        return "OK";
+                    }
+                }
+                catch (Exception e)
+                {
+                    //Console.WriteLine(e);
+                    continue;
+                }
+                finally
+                {
+                    i--;
+                }
+            }
+        }
+    }
+}
